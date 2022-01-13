b1d3483f5a17e9d1902d5478445f58f22c9f32af
mwchoen <mwcheon@cudo.co.kr>
2022-01-13 22:57:59 +0900

    상속 복습

---
+++ src/com/company/extendsTest/ExtendsMainTest.java
@@ -0,0 +1,12 @@
+package com.company.extendsTest;
+
+public class ExtendsMainTest {
+    public static void main(String []args ){
+
+        CheonCar cheonCar = new CheonCar("red", 1234, "45000");
+        cheonCar.isPower();
+
+
+
+    }
+}
