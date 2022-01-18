01be6a88d485796dcd0c0d064c574fddd5a54835
mwchoen <mwcheon@cudo.co.kr>
2022-01-18 09:01:11 +0900

    추상화 2

---
+++ src/com/company/ch07/abstractTest/VideoPlayer.java
@@ -0,0 +1,13 @@
+package com.company.ch07.abstractTest;
+
+public class VideoPlayer extends Player{
+    @Override
+    void playStart(int pos) {
+
+    }
+
+    @Override
+    void playStop() {
+
+    }
+}
---
+++ src/com/company/ch07/abstractUnit/Unit.java
@@ -0,0 +1,7 @@
+package com.company.ch07.abstractUnit;
+
+abstract public class Unit {
+    int x, y;
+    abstract public void move(int x, int y);
+    abstract public void stop();
+}
