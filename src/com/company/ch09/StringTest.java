f3710e7a7eafa9862f74d3b90704ce4b20f6cb30
mwchoen <cheonmyeongwon@gmail.com>
2022-02-02 22:53:25 +0900

    String

--- src/com/company/ch09/StringTest.java
+++ src/com/company/ch09/StringTest.java
@@ -8,7 +8,7 @@ public class StringTest {
         //String가 +로 합쳐질때마다 새로운 주소에 저장된다. 그래서 효율이 좋지 않음 ( Stringbuffer 을 사용 해야함 )
         // s1에는 새로운 주소값이 들어감
 
-        String emptyString = ""; // 빈문자열로 초기화
+        String emptyString = ""; // 빈문자열로 초기화  null로 초기화 하기보다는 빈문자열이 낫다.
 
 
     }
